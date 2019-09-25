6358ba3d406fb6171f614ecf82f336a1b6bfa269
Jennak21 <12jenna.k@gmail.com>
2019-09-26 00:24:32 +1200

    Remove unused imports

--- controllers/SelectAudioController.java
+++ controllers/SelectAudioController.java
@@ -1,31 +1,21 @@
 package controllers;
 
 
-
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
-
-import javax.swing.JOptionPane;
-import javax.swing.event.ChangeListener;
-
 import application.BashCommandClass;
 import application.Creation;
 import application.Main;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TableColumn;
@@ -34,15 +24,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DataFormat;
-import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 
-import javafx.fxml.Initializable;
 
 public class SelectAudioController extends SceneChanger implements Initializable{
 
